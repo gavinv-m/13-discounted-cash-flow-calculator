aa1286f63f87b2f98e9f440a3bda166489ee9df0
Gavin <gavinvm@protonmail.com>
2024-06-27 12:15:07 +0200

    Create discounted cash flow manager

--- src/application-logic/app-manager.js
+++ src/application-logic/app-manager.js
@@ -1,16 +1,19 @@
 import { dataCentre } from './data-centre/data-centre';
+import { dcfManager } from './dcf-manager/dcf-manager';
 
 class ApplicationManager {
-  constructor(dataCentre) {
+  constructor(dataCentre, dcfManager) {
     this.dataCentre = dataCentre;
+    this.dcfManager = dcfManager;
   }
 
   startApplication(tickerSymbol) {
     this.dataCentre.manageDataBase(tickerSymbol);
+    this.dcfManager.startProjections();
   }
 }
 
-const appManager = new ApplicationManager(dataCentre);
+const appManager = new ApplicationManager(dataCentre, dcfManager);
 
 // Exports to index.js
 export { appManager };
---
+++ src/application-logic/dcf-manager/dcf-manager.js
@@ -0,0 +1,15 @@
+class DiscountedCashFlowManager {
+  startingProjectionYear = null;
+  endingProjectionYear = null;
+
+  constructor() {}
+
+  startProjections() {}
+
+  // TODO: Determine start and end years
+}
+
+const dcfManager = new DiscountedCashFlowManager();
+
+// Exports to app-manager.js
+export { dcfManager };
