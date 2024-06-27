26c0c39089a400f23b5f3cf883959662fdb2579d
Gavin <gavinvm@protonmail.com>
2024-06-27 12:27:14 +0200

    Create revenue and expenses projections manager

--- src/application-logic/dcf-manager/dcf-manager.js
+++ src/application-logic/dcf-manager/dcf-manager.js
@@ -1,15 +1,19 @@
+import { revenueAndExpenses } from './projections/revenue-and-expenses-projections';
+
 class DiscountedCashFlowManager {
   startingProjectionYear = null;
   endingProjectionYear = null;
 
-  constructor() {}
+  constructor(revenueAndExpenses) {
+    this.revenueAndExpenses = revenueAndExpenses;
+  }
 
   startProjections() {}
 
   // TODO: Determine start and end years
 }
 
-const dcfManager = new DiscountedCashFlowManager();
+const dcfManager = new DiscountedCashFlowManager(revenueAndExpenses);
 
 // Exports to app-manager.js
 export { dcfManager };
---
+++ src/application-logic/dcf-manager/projections/revenue-and-expenses-projections.js
@@ -0,0 +1,10 @@
+class RevenueAndExpensesProjections {
+  projections = {};
+
+  constructor() {}
+}
+
+const revenueAndExpenses = new RevenueAndExpensesProjections();
+
+// Exports to dcf-manager.js
+export { revenueAndExpenses };
