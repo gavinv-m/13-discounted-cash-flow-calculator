359f481cef158b5eaaad859055244910e596cbf0
Gavin <gavinvm@protonmail.com>
2024-07-04 12:37:03 +0200

    Link instance of depreciation projections to dcf manager

--- src/application-logic/dcf-manager/dcf-manager.js
+++ src/application-logic/dcf-manager/dcf-manager.js
@@ -1,18 +1,27 @@
 import { revenueAndExpenses } from './projections/revenue-and-expenses-projections';
 import { projectionYears } from './projection-years-manager';
 import { capexProjectionsManager } from './projections/capex-projections';
+import { depreciationAmortisationProjectionsManager } from './projections/depreciation-amortisation-projections';
 
 class DiscountedCashFlowManager {
-  constructor(revenueAndExpenses, projectionYears, capexProjectionsManager) {
+  constructor(
+    revenueAndExpenses,
+    projectionYears,
+    capexProjectionsManager,
+    depreciationAmortisationProjectionsManager,
+  ) {
     this.revenueAndExpenses = revenueAndExpenses;
     this.projectionYearsManager = projectionYears;
     this.capexProjectionsManager = capexProjectionsManager;
+    this.depreciationAmortisationProjectionsManager =
+      depreciationAmortisationProjectionsManager;
   }
 
   startProjections() {
     this.projectionYearsManager.calculateProjectionYears();
     this.revenueAndExpenses.projectRevenueAndExpenses();
     this.capexProjectionsManager.calculateCapexProjections();
+    this.depreciationAmortisationProjectionsManager.projectDepreciationAmortisation();
   }
 }
 
@@ -20,6 +29,7 @@ const dcfManager = new DiscountedCashFlowManager(
   revenueAndExpenses,
   projectionYears,
   capexProjectionsManager,
+  depreciationAmortisationProjectionsManager,
 );
 
 // Exports to app-manager.js
---
+++ src/application-logic/dcf-manager/projections/depreciation-amortisation-projections.js
@@ -0,0 +1,13 @@
+class DepreciationAmortizationManager {
+  constructor() {}
+
+  projectDepreciationAmortisation() {
+    console.log('jasmine');
+  }
+}
+
+const depreciationAmortisationProjectionsManager =
+  new DepreciationAmortizationManager();
+
+// Exports to dcf-manager.js
+export { depreciationAmortisationProjectionsManager };
