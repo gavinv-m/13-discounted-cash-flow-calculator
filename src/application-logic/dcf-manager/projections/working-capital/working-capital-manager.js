eb1a8b46e7a316c09b47f80a415d6197dbcfa271
Gavin <gavinvm@protonmail.com>
2024-07-05 13:41:06 +0200

    Link accounts payable to working cap manager

--- src/application-logic/dcf-manager/projections/working-capital/working-capital-manager.js
+++ src/application-logic/dcf-manager/projections/working-capital/working-capital-manager.js
@@ -1,21 +1,24 @@
 import { accountsReceivableManager } from './accounts-receivable-projections';
+import { accountsPayableManager } from './accounts-payable-projections';
 
 class WorkingCapitalManager {
   projections = {};
 
-  constructor(accountsReceivableManager) {
+  constructor(accountsReceivableManager, accountsPayableManager) {
     this.accountsReceivableManager = accountsReceivableManager;
+    this.accountsPayableManager = accountsPayableManager;
   }
 
   projectWorkingCapital() {
     this.projections.accountsReceivable =
       this.accountsReceivableManager.projectAccountsReceivable();
-    console.log(this.projections);
+    this.accountsPayableManager.projectAccountsPayable();
   }
 }
 
 const workingCapProjectionsManager = new WorkingCapitalManager(
   accountsReceivableManager,
+  accountsPayableManager,
 );
 
 export { workingCapProjectionsManager };
