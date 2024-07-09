26a72d6dc8edffe8e834348111c3d38c738d78e4
Gavin <gavinvm@protonmail.com>
2024-07-09 10:57:30 +0200

    Link discount rate to terminal value manager

---
+++ src/application-logic/dcf-manager/projections/terminal-value/discount-rate-manager.js
@@ -0,0 +1,16 @@
+class DiscountRateManager {
+  discountRates = {};
+
+  constructor() {}
+
+  // Main method
+  calculateDiscountRates() {}
+
+  // TODO: Calculate WACC
+  // TODO: Calculate Long Term Growth Rate
+}
+
+const discountRatesManager = new DiscountRateManager();
+
+// Exports to terminal-value-manager.js
+export { discountRatesManager };
--- src/application-logic/dcf-manager/projections/terminal-value/terminal-value-manager.js
+++ src/application-logic/dcf-manager/projections/terminal-value/terminal-value-manager.js
@@ -1,23 +1,27 @@
 import { ebitdaCalculator } from './ebitda-manager';
 import { fcfManager } from './fcf-manager';
+import { discountRatesManager } from './discount-rate-manager';
 
 class TerminalValueManager {
   projections = {};
 
-  constructor(ebitdaCalculator, fcfManager) {
+  constructor(ebitdaCalculator, fcfManager, discountRatesManager) {
     this.ebitdaCalculator = ebitdaCalculator;
     this.fcfManager = fcfManager;
+    this.discountRatesManager = discountRatesManager;
   }
 
   calcuateSharePrice() {
     this.ebitdaCalculator.calculateEbitda();
     this.fcfManager.calculateFreeCashFlow();
+    this.discountRatesManager.calculateDiscountRates();
   }
 }
 
 const terminalValueManager = new TerminalValueManager(
   ebitdaCalculator,
   fcfManager,
+  discountRatesManager,
 );
 
 // Exports to dcf-manager.js
