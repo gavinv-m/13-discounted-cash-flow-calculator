b5c80d3b9089dba92382974d20e81398fd99f256
Gavin <gavinvm@protonmail.com>
2024-07-09 16:14:04 +0200

    Link growth rate to discount rate manager

--- src/application-logic/dcf-manager/projections/terminal-value/discount-rate-manager.js
+++ src/application-logic/dcf-manager/projections/terminal-value/discount-rate-manager.js
@@ -1,19 +1,25 @@
 import { waccManager } from './wacc-manager';
+import { growthRateManager } from './growth-rate-manager';
 
 class DiscountRateManager {
   discountRates = {};
 
-  constructor(waccManager) {
+  constructor(waccManager, growthRateManager) {
     this.waccManager = waccManager;
+    this.growthRateManager = growthRateManager;
   }
 
   // Main method
   calculateDiscountRates() {
     this.waccManager.calculateCostOfCapital();
+    this.growthRateManager.calculateGrowthRate();
   }
 }
 
-const discountRatesManager = new DiscountRateManager(waccManager);
+const discountRatesManager = new DiscountRateManager(
+  waccManager,
+  growthRateManager,
+);
 
 // Exports to terminal-value-manager.js
 export { discountRatesManager };
---
+++ src/application-logic/dcf-manager/projections/terminal-value/growth-rate-manager.js
@@ -0,0 +1,12 @@
+class GrowthRateManager {
+  growthRates = {};
+
+  constructor() {}
+
+  calculateGrowthRate() {}
+}
+
+const growthRateManager = new GrowthRateManager();
+
+// Exports to discount-rate-manager.js
+export { growthRateManager };
