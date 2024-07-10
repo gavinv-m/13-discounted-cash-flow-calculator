b73191bed5b66c40ecfbd2cdae259865fc7a1345
Gavin <gavinvm@protonmail.com>
2024-07-10 14:30:12 +0200

    Calculate terminal value

--- src/application-logic/dcf-manager/projections/terminal-value/terminal-value-calculator.js
+++ src/application-logic/dcf-manager/projections/terminal-value/terminal-value-calculator.js
@@ -1,21 +1,41 @@
 import { fcfManager } from './fcf-manager';
 import { growthRateManager } from './growth-rate-manager';
+import { waccManager } from './wacc-manager';
+import computeTerminalValue from '../../utils/calculate-terminal-value';
 
 class TerminalValueCalculator {
-  constructor(fcfManager, growthRateManager) {
+  projections = {};
+
+  constructor(
+    fcfManager,
+    growthRateManager,
+    waccManager,
+    computeTerminalValue,
+  ) {
     this.fcfManager = fcfManager;
     this.growthRateManager = growthRateManager;
+    this.waccManager = waccManager;
+    this.computeTerminalValue = computeTerminalValue;
   }
 
   calculateTerminalValue() {
     const freeCashFlows = this.fcfManager.sendData('freeCashFlow').freeCashFlow;
     const growthRate = this.growthRateManager.sendData('growthRate').growthRate;
+    const wacc = this.waccManager.sendData('wacc').wacc;
+
+    this.projections.terminalValue = this.computeTerminalValue(
+      freeCashFlows,
+      wacc,
+      growthRate,
+    );
   }
 }
 
 const terminalValueCalculator = new TerminalValueCalculator(
   fcfManager,
   growthRateManager,
+  waccManager,
+  computeTerminalValue,
 );
 
 // Exports to terminal-value-manager.js
---
+++ src/application-logic/dcf-manager/utils/calculate-terminal-value.js
@@ -0,0 +1,11 @@
+import { projectionYears } from '../projection-years-manager';
+
+// Exports to terminal-value-calculator.js
+export default function computeTerminalValue(freeCashFlows, wacc, growthRate) {
+  const endingProjectionYear = projectionYears.endingProjectionYear;
+  const endingFreeCashFlow = freeCashFlows[endingProjectionYear];
+  const terminalValue =
+    (endingFreeCashFlow * (1 + growthRate)) / growthRate - wacc;
+
+  return terminalValue;
+}
