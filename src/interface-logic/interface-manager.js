6e75937acdab5eeab55c96a25f4a4298ae8445e3
Gavin <gavinvm@protonmail.com>
2024-07-19 14:57:25 +0200

    Link interface manager to main controller

--- src/interface-logic/interface-manager.js
+++ src/interface-logic/interface-manager.js
@@ -1,5 +1,10 @@
 class InterfaceManager {
   constructor() {}
+
+  populateInterface() {}
 }
 
 const interfaceManager = new InterfaceManager();
+
+// Exports to main-controller.js
+export { interfaceManager };
--- src/main-controller.js
+++ src/main-controller.js
@@ -1,16 +1,19 @@
 import { appManager } from './application-logic/app-manager';
+import { interfaceManager } from './interface-logic/interface-manager';
 
 class MainController {
-  constructor(appManager) {
+  constructor(appManager, interfaceManager) {
     this.appManager = appManager;
+    this.interfaceManager = interfaceManager;
   }
 
   initializeApp(tickerSymbol) {
     this.appManager.startApplication(tickerSymbol);
+    this.interfaceManager.populateInterface();
   }
 }
 
-const mainController = new MainController(appManager);
+const mainController = new MainController(appManager, interfaceManager);
 
 // Exports to index.js
 export { mainController };
