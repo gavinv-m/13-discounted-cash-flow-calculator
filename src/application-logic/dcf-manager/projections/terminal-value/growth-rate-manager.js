d31bd8a268b92ce6675a27744b9d24550758ce41
Gavin <gavinvm@protonmail.com>
2024-07-22 10:14:53 +0200

    Store long term growth rate used

--- src/application-logic/dcf-manager/projections/terminal-value/growth-rate-manager.js
+++ src/application-logic/dcf-manager/projections/terminal-value/growth-rate-manager.js
@@ -1,6 +1,7 @@
 import { cashFlowStatementDataManager } from '../../../data-centre/refined-data/cash-flow-statement';
 import { overviewDataManager } from '../../../data-centre/refined-data/overview';
 import { customInputManager } from '../../../data-centre/custom-inputs/custom-input-manager';
+import { activeMetrics } from '../../../data-centre/active-metrics/active-metrics';
 import calculateExpectedGrowthRate from '../../utils/calculate-growth-rate';
 import getFinancialLineItems from '../../../data-centre/utils/financial-data-utils';
 
@@ -11,12 +12,14 @@ class GrowthRateManager {
     cashFlowStatementDataManager,
     overviewDataManager,
     customInputManager,
+    activeMetrics,
     calculateExpectedGrowthRate,
     getFinancialLineItems,
   ) {
     this.cashFlowStatementDataManager = cashFlowStatementDataManager;
     this.overviewDataManager = overviewDataManager;
     this.customInputManager = customInputManager;
+    this.activeMetrics = activeMetrics;
     this.calculateExpectedGrowthRate = calculateExpectedGrowthRate;
     this.getGrowthRate = getFinancialLineItems.bind(this);
   }
@@ -31,6 +34,10 @@ class GrowthRateManager {
 
     if (customGrowthRate !== null) {
       this.growthRates.growthRate = customGrowthRate / 100;
+      this.activeMetrics.setMetric(
+        'longTermGrowthRate',
+        this.growthRates.growthRate * 100,
+      );
       return;
     }
 
@@ -51,6 +58,10 @@ class GrowthRateManager {
       dividends,
       returnOnEquity,
     );
+    this.activeMetrics.setMetric(
+      'longTermGrowthRate',
+      this.growthRates.growthRate * 100,
+    );
   }
 }
 
@@ -58,6 +69,7 @@ const growthRateManager = new GrowthRateManager(
   cashFlowStatementDataManager,
   overviewDataManager,
   customInputManager,
+  activeMetrics,
   calculateExpectedGrowthRate,
   getFinancialLineItems,
 );
