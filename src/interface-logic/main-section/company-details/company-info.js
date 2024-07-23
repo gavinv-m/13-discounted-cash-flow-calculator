fe64200c0bcc61a9c06f23e1c75abad834a0081c
Gavin <gavinvm@protonmail.com>
2024-07-23 13:21:27 +0200

    Link company info to main section manager

---
+++ src/interface-logic/main-section/company-details/company-info.js
@@ -0,0 +1,10 @@
+class CompanyInfoManager {
+  constructor() {}
+
+  populateCompanyInfoSection() {}
+}
+
+const companyInfoManager = new CompanyInfoManager();
+
+// Exports to main-section-manager.js
+export { companyInfoManager };
--- src/interface-logic/main-section/main-section-manager.js
+++ src/interface-logic/main-section/main-section-manager.js
@@ -1,16 +1,22 @@
 import { dcfInfoManager } from './dcf-info/dcf-info';
+import { companyInfoManager } from './company-details/company-info';
 
 class MainSectionManager {
-  constructor(dcfInfoManager) {
+  constructor(dcfInfoManager, companyInfoManager) {
     this.dcfInfoManager = dcfInfoManager;
+    this.companyInfoManager = companyInfoManager;
   }
 
   populateMainSection() {
     this.dcfInfoManager.populateDCFInfoSection();
+    this.companyInfoManager.populateCompanyInfoSection();
   }
 }
 
-const mainSectionManager = new MainSectionManager(dcfInfoManager);
+const mainSectionManager = new MainSectionManager(
+  dcfInfoManager,
+  companyInfoManager,
+);
 
 // Exports to interface-manager.js
 export { mainSectionManager };
