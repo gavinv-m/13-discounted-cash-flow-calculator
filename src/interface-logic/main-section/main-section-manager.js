532289e804aa2461edb52d8c0e379ab8ab4f7720
Gavin <gavinvm@protonmail.com>
2024-07-24 16:38:03 +0200

    Link valuation financials to main section manager

--- src/interface-logic/main-section/main-section-manager.js
+++ src/interface-logic/main-section/main-section-manager.js
@@ -1,22 +1,28 @@
 import { dcfInfoManager } from './dcf-info/dcf-info';
 import { companyInfoManager } from './company-details/company-info';
+import { valuationFinancialsManager } from './valuation-financials/valuation-financials';
 
 class MainSectionManager {
-  constructor(dcfInfoManager, companyInfoManager) {
+  constructor(dcfInfoManager, companyInfoManager, valuationFinancialsManager) {
     this.mainContent = document.getElementById('main-content');
     this.dcfInfoManager = dcfInfoManager;
     this.companyInfoManager = companyInfoManager;
+    this.valuationFinancialsManager = valuationFinancialsManager;
   }
 
   populateMainSection() {
     this.dcfInfoManager.populateDCFInfoSection(this.mainContent);
     this.companyInfoManager.populateCompanyInfoSection(this.mainContent);
+    this.valuationFinancialsManager.populateValuationFinancialsSection(
+      this.mainContent,
+    );
   }
 }
 
 const mainSectionManager = new MainSectionManager(
   dcfInfoManager,
   companyInfoManager,
+  valuationFinancialsManager,
 );
 
 // Exports to interface-manager.js
---
+++ src/interface-logic/main-section/valuation-financials/valuation-financials.js
@@ -0,0 +1,10 @@
+class ValuationFinancialsManager {
+  constructor() {}
+
+  populateValuationFinancialsSection() {}
+}
+
+const valuationFinancialsManager = new ValuationFinancialsManager();
+
+// Exports to main-section-manager.js
+export { valuationFinancialsManager };
