98dce08c9d2bd815bc21a7b5ef6e9310a31978a8
Gavin <gavinvm@protonmail.com>
2024-07-24 15:46:57 +0200

    Add company details from overview

--- src/interface-logic/main-section/company-details/utils/render-search-section.js
+++ src/interface-logic/main-section/company-details/utils/render-search-section.js
@@ -3,6 +3,7 @@ import {
   createElement,
   createInput,
 } from '../../../utils/element-utils';
+import { overviewDataManager } from '../../../../application-logic/data-centre/refined-data/overview';
 
 const createSearch = function createSearchBoxErrorContainer() {
   const searchBoxAndError = createElement('div', { id: 'search-box-error' });
@@ -22,16 +23,42 @@ const createDetails = function createCompanyNameSectorContainer() {
     id: 'company-details',
   });
 
-  const companyNameAndTickerContainer = createElement('div'); // Top half of companyNameSectorContainer
-  const ticker = createElement('h1', { classList: ['ticker-symbol'] });
-  const companyName = createElement('h1', { id: 'company-full-name' });
-  appendChildren(companyNameAndTickerContainer, ticker, companyName);
+  // Top half of companyNameSectorContainer
+  const companyNameAndTickerContainer = createElement('div');
+
+  const tickerSymbol = overviewDataManager.sendData('Symbol').Symbol;
+  const tickerContainer = createElement('h1', {
+    text: tickerSymbol,
+    classList: ['ticker-symbol'],
+  });
+
+  const companyName = overviewDataManager.sendData('Name').Name;
+  const companyNameContainer = createElement('h1', {
+    text: companyName,
+    id: 'company-full-name',
+  });
+  appendChildren(
+    companyNameAndTickerContainer,
+    tickerContainer,
+    companyNameContainer,
+  );
   companyNameSectorContainer.appendChild(companyNameAndTickerContainer);
 
-  const sectorIndustryContainer = createElement('div'); // Bottom half of companyNameSectorContainer
-  const sector = createElement('h1', { id: 'company-sector' });
-  const industry = createElement('h1', { id: 'company-industry' });
-  appendChildren(sectorIndustryContainer, sector, industry);
+  // Bottom half of companyNameSectorContainer
+  const sectorIndustryContainer = createElement('div');
+
+  const sectorName = overviewDataManager.sendData('Sector').Sector;
+  const sectorContainer = createElement('h1', {
+    text: sectorName,
+    id: 'company-sector',
+  });
+
+  const industryName = overviewDataManager.sendData('Industry').Industry;
+  const industryContainer = createElement('h1', {
+    text: industryName,
+    id: 'company-industry',
+  });
+  appendChildren(sectorIndustryContainer, sectorContainer, industryContainer);
   companyNameSectorContainer.appendChild(sectorIndustryContainer);
 
   return companyNameSectorContainer;
