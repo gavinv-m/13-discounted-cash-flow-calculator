d91923c70e5721b9d5dd717a311e081d82024ca1
Gavin <gavinvm@protonmail.com>
2024-07-25 13:38:21 +0200

    Link valuation content manager to valuation financials manager

---
+++ src/interface-logic/main-section/valuation-financials/info-section-content/valuation-content/valuation-content-manager.js
@@ -0,0 +1,10 @@
+class ValuationContentManager {
+  constructor() {}
+
+  addValuationContent(valFinInfoSection) {}
+}
+
+const valuationContentManager = new ValuationContentManager();
+
+// Exports to valuation-financials-manager.js and valuation-heading-listener
+export { valuationContentManager };
--- src/interface-logic/main-section/valuation-financials/valuation-financials-manager.js
+++ src/interface-logic/main-section/valuation-financials/valuation-financials-manager.js
@@ -1,22 +1,28 @@
 import rendervalFinSection from './utils/render-valuation-financials-section';
 import renderValFinHeadings from './utils/render-headings';
 import renderValFinInfoSection from './utils/render-val-fin-info-section';
+import { valuationContentManager } from './info-section-content/valuation-content/valuation-content-manager';
 
 class ValuationFinancialsManager {
   constructor(
     rendervalFinSection,
     renderValFinHeadings,
     renderValFinInfoSection,
+    valuationContentManager,
   ) {
     this.rendervalFinSection = rendervalFinSection;
     this.renderValFinHeadings = renderValFinHeadings;
     this.renderValFinInfoSection = renderValFinInfoSection;
+    this.valuationContentManager = valuationContentManager;
   }
 
   populateValuationFinancialsSection(mainContent) {
     const valFinSection = this.rendervalFinSection(mainContent);
     this.renderValFinHeadings(valFinSection);
     const infoContentContainer = this.renderValFinInfoSection(valFinSection);
+
+    // Add valuation content on load
+    this.valuationContentManager.addValuationContent(infoContentContainer);
   }
 }
 
@@ -24,6 +30,7 @@ const valuationFinancialsManager = new ValuationFinancialsManager(
   rendervalFinSection,
   renderValFinHeadings,
   renderValFinInfoSection,
+  valuationContentManager,
 );
 
 // Exports to main-section-manager.js
