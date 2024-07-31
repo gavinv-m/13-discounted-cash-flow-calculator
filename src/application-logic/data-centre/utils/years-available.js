62e62b667a644254b5a9c626b269dc330068d2b3
Gavin <gavinvm@protonmail.com>
2024-07-31 10:54:21 +0200

    Store years with months

--- src/application-logic/data-centre/utils/years-available.js
+++ src/application-logic/data-centre/utils/years-available.js
@@ -1,14 +1,30 @@
 export default function getYearsAvailable(statementData) {
   const annualReports = statementData.annualReports;
+  const monthMap = {
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dec',
+  };
   const years = [];
 
   annualReports.forEach((year) => {
-    const fiscalYear = year.fiscalDateEnding.split('-')[0];
-    years.push(fiscalYear);
+    const fiscalYear = year.fiscalDateEnding.split('-');
+    const text = `${monthMap[fiscalYear[1]]}-${fiscalYear[0]}`;
+    years.push(text);
   });
 
-  const latestToOldest = [...years].sort((a, b) => b - a);
-  const oldestToLatest = [...years].sort((a, b) => a - b);
+  const latestToOldest = years;
+  const oldestToLatest = [...years].reverse();
+  console.log(latestToOldest, oldestToLatest);
 
   return {
     latestToOldest,
